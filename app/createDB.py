9f2c5ee46953d6b3273132ba8227d9937e96499f
aitbenha <a.aitbenha@cirestechnologies.ma>
2023-04-07 12:35:12 +0000

    Integration with exchanges

--- app/createDB.py
+++ app/createDB.py
@@ -1,7 +1,7 @@
 from gate_ways.alchemy_db_create import Db_creator
 from gate_ways.config_handler import Config_handler
 
-
+#creating database
 config = Config_handler()
 pg = Db_creator(config)
 pg.create()
