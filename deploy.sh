0d844265c9e146eda0fcd623935803b3832f20ca
aitbenha <a.aitbenha@cirestechnologies.ma>
2023-04-07 11:40:45 +0000

    Integration with exchanges

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# check for changes in the git repository
+# change working directory to your Git repository directory
+cd /home/Tsignal
+
+# check if there are changes in the Git repository
 if [ -n "$(git diff --exit-code)" ]; then
   # if there are changes, stop and remove existing containers
   docker-compose down
@@ -8,14 +11,14 @@ if [ -n "$(git diff --exit-code)" ]; then
   # remove existing images
   docker images | grep "tsignal" | awk '{print $3}' | xargs docker rmi
 
-  # pull latest code from git repository
+  # pull latest code from Git repository
   git pull
 
   # build new image
   docker build -t tsignal .
 else
   # if there are no changes, do nothing
-  echo "No changes in git repository, skipping build and deploy."
+  echo "No changes in Git repository, skipping build and deploy."
 fi
 
 # start containers
