bca65b87b328114d8d8cd692d54f44297ef00517
aitbenha <a.aitbenha@cirestechnologies.ma>
2023-04-08 15:10:25 +0000

    change symbol to base quote

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # change working directory to your Git repository directory
-cd /home/Tsignal
+cd /home/Tsignal/gitRepo
 
-# check if there are changes in the Git repository
-if cd /home/Tsignal/gitRepo && git diff --exit-code >/dev/null; then
+# fetch the latest changes from remote repository
+git fetch
+
+# check if the local branch is behind the remote branch
+if [ $(git status -uno | grep 'Your branch is behind' | wc -l) -eq 0 ]; then
 
   # if there are no changes, do nothing
   echo "No changes in Git repository, skipping build and deploy."
  
 else
 
-   echo "changes detected"
+   echo "Changes detected"
    # if there are changes, stop and remove existing containers
   docker-compose down
 
@@ -26,7 +29,7 @@ else
   # build new image
   docker build -t tsignal .
 
-fi
+  # start containers
+  docker-compose up -d
 
-# start containers
-docker-compose up -d
+fi
