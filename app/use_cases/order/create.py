b401f2822335a10f8c7a61c8d404e8014e474221
aitbenha <a.aitbenha@cirestechnologies.ma>
2023-05-14 23:53:17 +0000

    version2

--- app/use_cases/order/create.py
+++ app/use_cases/order/create.py
@@ -26,21 +26,24 @@ class Create:
     def handle(self, order:Order , key:str):
         with self.sessionContext as session:
             strategy = self.strategyRepo.getStrategyByWebhookId(session , order.strategy_id)
-            if strategy.webhook_key == key:
-                order.reception_date = datetime.now()
-                account = self.accountRepo.getAccountById(session , strategy.account_id)
-                order.id = str(uuid.uuid4())
-                
-                account.key = self.secretRepo.read(account.key_id)
-                exchange = ExchangeExecution(account.exchange.id , account.key)
-                response = exchange.executeOrder(account.exchange.id , order)
-                order.response  =  str(response)
-                order.execution_date = datetime.now()
-                self.orderRepo.save(session, order)
-                logger.log(response)
-                return response
+            if not strategy is None:
+                if strategy.webhook_key == key:
+                    order.reception_date = datetime.now()
+                    account = self.accountRepo.getAccountById(session , strategy.account_id)
+                    order.id = str(uuid.uuid4())
+                    
+                    account.key = self.secretRepo.read(account.key_id)
+                    exchange = ExchangeExecution(account.exchange.id , account.key)
+                    response = exchange.executeOrder(account.exchange.id , order)
+                    order.response  =  str(response)
+                    order.execution_date = datetime.now()
+                    self.orderRepo.save(session, order)
+                    logger.log(response)
+                    return response
+                else :
+                    raise  Exception("invalide key")
             else :
-                raise  Exception("invalide key")
+                raise  Exception("unkown strategy")
 
 
 
