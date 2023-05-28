9b6f0d96ce06976b5da921a71b7345ad2fe84f96
aitbenha <a.aitbenha@cirestechnologies.ma>
2023-05-28 08:46:01 +0000

    dashboard

--- app/forms/dashboard/userHomeResponse.py
+++ app/forms/dashboard/userHomeResponse.py
@@ -3,7 +3,19 @@ from dataclasses import dataclass, asdict
 
 @dataclass
 class UserHomeResponse:
-    total_orders: float = None
+    total_orders: int = None
+
+    total_sell_orders : int = None
+    total_buy_orders: int = None
+
+    average_sell_price: float = None 
+    average_buy_price: float = None
+
+    total_sell_quantitiy: float = None
+    total_buy_quantity: float = None
+
+    monthly_profit = [0 for _ in range(12)]
+
 
 
     @classmethod
