f5e4018495cfe5e30c1548572b6d4d05376862b0
aitbenha <a.aitbenha@cirestechnologies.ma>
2023-05-29 10:37:03 +0000

    get all exchanges

--- app/controllers/exchangeController.py
+++ app/controllers/exchangeController.py
@@ -21,7 +21,7 @@ get_all_handler = GetAll(exchange_repo)
 def getAll():
     try:
         data = get_all_handler.handler(GetAllInput(all="all"))
-        json_data = json.dumps(data.to_dict())
+        json_data = json.dumps([ _ for _ in data.to_dict()])
         return Response(json_data ,  status=200, mimetype='application/json')
     except Exception as e :
         json_data = json.dumps({"status_message":str(e)})
