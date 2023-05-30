336827f8151ce4b9778cf8bdfa885aee98d72e64
aitbenha <a.aitbenha@cirestechnologies.ma>
2023-05-30 13:20:56 +0000

    get order details

--- app/use_cases/order/getDetails.py
+++ app/use_cases/order/getDetails.py
@@ -11,9 +11,20 @@ class GetDetails:
 
     def handle(self, order_id:str):
         with self.sessionContext as session : 
+            
             order = self.order_repo.getOrderById(session , order_id)
+            if order is None:
+                raise Exception("Order Not found")
+            
+            if order.execution_id is None:
+                raise Exception("The order has not been executed")
+            
             strategy = self.strategy_repo.getStrategyById(session ,order.strategy_id)
+            if strategy is None:
+                raise Exception("Unknown Strategy")
             account = self.account_repo.getAccountById(session , strategy.account_id)
+            if account is None:
+                raise Exception("Unknown wallet")
             exchange = ExchangeExecution(account.exchange.id, account.key)
             response = exchange.getOrderDetails(order_id)
             return response
