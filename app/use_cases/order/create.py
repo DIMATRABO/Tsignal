c46e58cb82623f15a8069f86ec668a68c6fec5a8
aitbenha <a.aitbenha@cirestechnologies.ma>
2023-05-31 12:35:35 +0000

    map order response data to order entity

--- app/use_cases/order/create.py
+++ app/use_cases/order/create.py
@@ -54,10 +54,21 @@ class Create:
 
     def map_data(self , order , response ):
         if not response is None:
-            order.response  =  str(response)
-            order.execution_id = response['id']
-            order.status = response["status"]
-            order.execution_price = response['average']
+            order.response  =  str(response) 
+            if "id" in response:
+                order.execution_id = response['id'] 
+            else:
+                order.execution_id = None
+            
+            if "status" in response:
+                order.status = response['status'] 
+            else:
+                order.status = 'failed'
+
+            if "average" in response:
+                order.execution_price = response['average'] 
+            else:
+                order.execution_price = None
 
             if "datetime" in response:
                 order.execution_date = datetime.now() if response['datetime'] == None else datetime.strptime(response["datetime"], "%Y-%m-%dT%H:%M:%S.%fZ")
