d4a09d5ea3ac6980a6308417358c3e6497cccd80
aitbenha <a.aitbenha@cirestechnologies.ma>
2023-06-06 08:23:09 +0000

    stats

--- app/controllers/strategyController.py
+++ app/controllers/strategyController.py
@@ -93,6 +93,7 @@ def getStrategiesByUserId():
     try:
         userId = get_jwt()["userId"]
         strategies = getAllAdvanced.handle(user_id=userId)
+        raise Exception(strategies)
         json_data = json.dumps([strategy.to_dict() for strategy in strategies] )
         return Response(json_data, status = 200, mimetype='application/json')
       
