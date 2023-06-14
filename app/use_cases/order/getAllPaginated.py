525d06f2e4f0edc15f2babb55f1a9af7686544ee
aitbenha <a.aitbenha@cirestechnologies.ma>
2023-06-14 20:28:54 +0000

    pagination

--- app/use_cases/order/getAllPaginated.py
+++ app/use_cases/order/getAllPaginated.py
@@ -7,11 +7,10 @@ class GetAllPaginated:
         self.sessionContext = SessionContext() 
 
     def handle(self, getOrdersInput, page_number, page_size):
-        raise Exception(getOrdersInput.webhook_id )
         with self.sessionContext as session:
             if ( not getOrdersInput.webhook_id is None ) and (not getOrdersInput.user_id is None): 
                 to_return = self.repo.getAllByStrategyIdAndUserIdPaginated(
-                    session, getOrdersInput.webhook_id, None, page_number, page_size
+                    session, getOrdersInput.webhook_id, getOrdersInput.user_id, page_number, page_size
                 )
             if ( getOrdersInput.webhook_id is None ) and (not getOrdersInput.user_id is None): 
                  to_return = self.repo.getAllByUserIdPaginated(
