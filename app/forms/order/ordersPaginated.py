7296b597a0f30e442342103f5fac148f3e5af1b8
aitbenha <a.aitbenha@cirestechnologies.ma>
2023-06-14 20:04:30 +0000

    pagination

--- app/forms/order/ordersPaginated.py
+++ app/forms/order/ordersPaginated.py
@@ -11,4 +11,5 @@ class OrdersPage:
     orders: List[Order] = field(default_factory=list)
 
     def to_dict(self):
+        self.orders = [order.to_dict() for order in self.orders]
         return asdict(self)
