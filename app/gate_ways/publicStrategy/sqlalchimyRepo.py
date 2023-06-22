ce31fb09e81a11bbb2e1249bbc1ff25c2238b8f2
aitbenha <a.aitbenha@cirestechnologies.ma>
2023-06-22 14:01:21 +0000

    public strategies

--- app/gate_ways/publicStrategy/sqlalchimyRepo.py
+++ app/gate_ways/publicStrategy/sqlalchimyRepo.py
@@ -28,3 +28,45 @@ class SqlAlchimy_repo :
          
         return publicStrategyEntity.to_domain()
         
+
+
+    def update(self, session , publicStrategy:PublicStrategy):
+        publicStrategyEntity = PublicStrategyEntity()
+        publicStrategyEntity.from_domain(model=publicStrategy)
+        
+        session.merge(publicStrategyEntity)
+        try:        
+            session.commit()
+        except exc.SQLAlchemyError as e:
+            logger.log(e)
+            session.rollback()
+            raise Exception("publicStrategy not updated")
+         
+      
+    """ 
+    def deleteUsersPublicStrategy(self, session, publicStrategy, user_id):
+        try:
+            # Delete associated orders first
+            session.query(OrderEntity).filter(OrderEntity.publicStrategy_id == publicStrategy.webhook_id, PublicStrategyEntity.account_id == AccountEntity.id, AccountEntity.user_id == user_id).delete(synchronize_session='fetch')
+
+            # Delete the publicStrategy only if it belongs to the user
+            num_deleted = session.query(PublicStrategyEntity).filter(PublicStrategyEntity.id == publicStrategy.id, PublicStrategyEntity.account_id == AccountEntity.id, AccountEntity.user_id == user_id).delete(synchronize_session='fetch')
+            if num_deleted == 0:
+                # Handle case where no matching records were found
+                raise Exception("No matching records found for publicStrategy ID {} belonging to user ID {}".format(publicStrategy.id, user_id))
+
+            session.commit()
+        except exc.SQLAlchemyError as e:
+            session.rollback()
+            raise Exception("Error deleting publicStrategy with ID {} belonging to user ID {}: {}".format(publicStrategy.id, user_id, str(e)))
+    """
+
+    def getAllStrategies(self, session):
+        strategies = session.query(PublicStrategyEntity).all()
+        return [publicStrategy.to_domain() for publicStrategy in strategies]
+      
+ 
+    def getPublicStrategyById(self, session , uuid):
+        publicStrategy = session.query(PublicStrategyEntity).filter(PublicStrategyEntity.id == uuid).first()
+        return None if publicStrategy == None else publicStrategy.to_domain()
+    
